0639635857f30bfd192ce29bb19ad36cf4f8afcd
서지원 <readyjiwon@gmail.com>
2023-06-20 11:03:09 +0900

    initial commit

---
+++ src/main/java/com/site/bemystory/BemystoryApplication.java
@@ -0,0 +1,13 @@
+package com.site.bemystory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemystoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BemystoryApplication.class, args);
+    }
+
+}
