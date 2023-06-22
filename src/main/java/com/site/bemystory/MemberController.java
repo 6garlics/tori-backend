f883ad640dc5ca833c5c3e9d5858b48b47be70ed
서지원 <readyjiwon@gmail.com>
2023-06-22 15:38:43 +0900

    리액트 연결 실습

---
+++ src/main/java/com/site/bemystory/MemberController.java
@@ -0,0 +1,21 @@
+package com.site.bemystory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/site/bemystory/MemberForm.java
@@ -0,0 +1,11 @@
+package com.site.bemystory;
+
+public class MemberForm {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
