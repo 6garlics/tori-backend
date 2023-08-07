e38342a524a73b635450c29d39cb31b21d924f4c
서지원 <readyjiwon@gmail.com>
2023-08-07 15:00:03 +0900

    add log on UserConroller

--- src/main/java/com/site/bemystory/controller/UserController.java
+++ src/main/java/com/site/bemystory/controller/UserController.java
@@ -4,6 +4,7 @@ import com.site.bemystory.dto.UserJoinRequest;
 import com.site.bemystory.dto.UserLoginRequest;
 import com.site.bemystory.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/users")
+@Slf4j
 @RequiredArgsConstructor
 public class UserController {
 
@@ -21,12 +22,14 @@ public class UserController {
     public ResponseEntity<String> join(@RequestBody UserJoinRequest dto){
 
         userService.join(dto.getUserName(), dto.getPassword(), dto.getEmail());
+        log.info("{}님 회원가입 성공", dto.getUserName());
         return ResponseEntity.ok().body("회원가입 성공했습니다.");
     }
 
     @PostMapping("/users/login")
     public ResponseEntity<String> login(@RequestBody UserLoginRequest dto){
         String token = userService.login(dto.getUserName(), dto.getPassword());
+        log.info("{}님 로그인 성공", dto.getUserName());
         return ResponseEntity.ok().body(token);
     }
 }
