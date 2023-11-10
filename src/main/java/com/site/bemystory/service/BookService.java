00aab5f191505ba6358ed411aa9b920f2bb75856
서지원 <readyjiwon@gmail.com>
2023-11-10 15:20:31 +0900

    [test] cat 책장 수리

--- src/main/java/com/site/bemystory/service/BookService.java
+++ src/main/java/com/site/bemystory/service/BookService.java
@@ -163,6 +163,7 @@ public class BookService {
      */
     public String findCover(Book book) {
         Cover cover = coverRepository.findByBook(book).get();
+        log.info("{} cover 찾음", book.getBookId());
         return cover.getUrl();
     }
 
